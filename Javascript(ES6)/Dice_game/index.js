3461ab10d96c2b26f9b166d0a411eaa041a66ae2
KrishnashishMunshi <ksmunshi06@gmail.com>
2025-11-02 20:51:26 +0530

    Dice game

---
+++ Javascript(ES6)/Dice_game/index.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+
+
+var dice = "images/dice"+randomNumber1+".png";
+document.getElementsByClassName("img1")[0].setAttribute("src", dice);
+
+var dice2 = "images/dice"+randomNumber2+".png";
+document.getElementsByClassName("img2")[0].setAttribute("src", dice2);
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+
+if(randomNumber1===randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw!";
+}
